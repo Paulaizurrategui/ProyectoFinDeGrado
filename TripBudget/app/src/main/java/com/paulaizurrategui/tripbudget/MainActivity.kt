90213defffa744d61d2aa8646c05536822463ba8
Paula Izurrategui López <paula.izurrategui.lopez@gregoriofer.com>
2026-03-17 20:32:41 +0100

    Creación del proyecto - Vacio

---
+++ TripBudget/app/src/main/java/com/paulaizurrategui/tripbudget/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.paulaizurrategui.tripbudget
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.paulaizurrategui.tripbudget.ui.theme.TripBudgetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TripBudgetTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TripBudgetTheme {
+        Greeting("Android")
+    }
+}
